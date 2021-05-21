704f3ee2f8a2f6b612fde316fa3a59e56b661dd3
Yαƚσ <75163978+Nksama@users.noreply.github.com>
2021-05-21 10:31:05 -0400

    Update and rename sample_config.py to config.py

--- thebot/config.py
+++ thebot/config.py
@@ -1,9 +1,10 @@
+import os
 class Config(object):
     LOGGER = True
-    API_ID = 69
-    API_HASH = "owo"
-    TOKEN = "uwu"
-    DB_URI = ""
+    API_ID = os.environ.get("API_ID", None)
+    API_HASH = os.environ.get("API_HASH", None)
+    TOKEN = os.environ.get("TOKEN" , Nome)
+    DB_URI = os.environ.get("DB" , None)
     LOG_CHANNEL = 0 # if you want a logging channel you can add this, else logs will go into Owner's PM
 
 class Development(Config):
